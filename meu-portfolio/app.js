2b701a8e52cee256814f46c73998f6106ab32c2d
Lavínia Piratello <145591879+laviniappiratello@users.noreply.github.com>
2025-04-19 14:11:35 -0300

    Update app.js

--- meu-portfolio/app.js
+++ meu-portfolio/app.js
@@ -71,7 +71,7 @@ app.get('/projetos', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 8080; 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
